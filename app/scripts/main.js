964e855411d1750ade752d360348273a58166032
Chyngyz Arystan uulu <camphosis@gmail.com>
2015-12-02 03:07:13 +0600

    Verstka

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+;(function ($) {
+	$(document).ready(function() {
+		
+	    if (!Modernizr.video || $(window).width() < 780) {
+
+	    } else {
+	    	setTimeout(function() { 
+	    		BV = new $.BigVideo({container: $('.video-bg')});
+		    	BV.init();
+		        BV.show('media/bwm7.mp4', {ambient:true});
+	    	}, 3500);
+
+	    	
+	    }
+
+	    $("#bwm-slider-1, #bwm-slider-2").owlCarousel({
+ 
+		      navigation : true, // Show next and prev buttons
+		      slideSpeed : 300,
+		      paginationSpeed : 400,
+		      singleItem:true,
+		      navigationText: false
+	 
+	  	});
+
+	});
+})(jQuery);
